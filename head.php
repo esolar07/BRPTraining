5bd7da8a3efe68aedbdf8ccad81b6a64424d7eca
esolar07 <esolar07@gmail.com>
2014-11-16 03:44:07 -0500

    BRP Training 2

---
+++ head.php
@@ -0,0 +1,12 @@
+
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php echo "BRPTraining -".$page; ?></title>
+    <link rel="stylesheet" href="stylesheets/app.css" />
+    <script src="bower_components/modernizr/modernizr.js"></script>
+  </head>
+
+  <?php include_once "nav.php" ?>
---
+++ nav.php
@@ -0,0 +1,22 @@
+
+<!-- Top Header -->
+<header>
+	<div>
+		<div class="brp-logo">
+			<img src='images/BRPlogo.png' />
+		</div>
+		
+		<!-- Nav Bar -->
+		<nav>
+			<ul>
+				<li><a href="#">HOME</a></li>
+				<li><a href="#">VISION</a></li>
+				<li><a href="#">MEET BRP</a></li>
+				<li><a href="#">SERVICES</a></li>
+				<li><a href="#">BOOK NOW</a></li>
+			</ul>
+		</nav>
+		<!-- Nav Bar Ends -->
+	</div>
+</header>
+<!-- Top Header Ends -->
