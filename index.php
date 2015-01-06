953e266cebfc484e22d616c9c266835e4250cb32
esolar07 <esolar07@gmail.com>
2015-01-05 23:53:15 -0500

    added php splash code. fixed layout issues

--- index.php
+++ index.php
@@ -3,22 +3,16 @@
 <!-- Includes Head -->
   <?php include_once "head.php"; ?>
 
-      <!-- Splash Page begins -->
-    <div class="splash">
-      <audio src="http://turbohackezin.free.fr/music/New/Kanye%20West%20-%20Graduation%20-%20Stronger.mp3" preload="auto" autoplay></audio>
-      <div class="imgRow ">
-        <div class="imgCol">
-          <img alt="logo" src="images/BRPlogo.png"/>
-        </div>
-      </div>
-      <div id ="sertop" class="butRow">
-        <div class="butCol">
-          <h1><a href="#">Start Believing</a></h1>
-        </div>
-      </div>
-    </div>
-    
-  <!-- Splash Page ends -->
+  <?php
+
+  // displays spalsh page if cookie is not set
+  if (!isset($_COOKIE["visited"])){
+    include_once "splash.php";
+    // sets cookie and spalsh is no longer displayed
+    setcookie("visited", TRUE, time() + 84600, "/");
+  }
+
+  ?>
 
     <div class="container">
       <!-- slide begins-->
---
+++ splash.php
@@ -0,0 +1,16 @@
+    <!-- Splash Page begins -->
+    <div class="splash">
+      <audio src="http://turbohackezin.free.fr/music/New/Kanye%20West%20-%20Graduation%20-%20Stronger.mp3" preload="auto" autoplay></audio>
+      <div class="imgRow ">
+        <div class="imgCol">
+          <img alt="logo" src="images/BRPlogo.png"/>
+        </div>
+      </div>
+      <div id ="sertop" class="butRow">
+        <div class="butCol">
+          <h1><a href="#">Start Believing</a></h1>
+        </div>
+      </div>
+    </div>
+    
+  <!-- Splash Page ends -->
